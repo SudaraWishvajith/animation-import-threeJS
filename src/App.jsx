789451a9640d91d4cfe73edaa21619f705d71ea7
SudaraWishvajith <it21221378@my.sliit.lk>
2023-07-23 19:37:53 +0530

    create context

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Experience } from "./components/Experience";
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+    <Canvas camera={{ position: [1, 1.5, 2.5], fov: 50 }} shadows>
       <color attach="background" args={["#ececec"]} />
       <Experience />
     </Canvas>
---
+++ src/context/CharactorAnimaions.jsx
@@ -0,0 +1,23 @@
+import { createContext, useContext, useState } from "react";
+
+const CharacterAnimationsContext = createContext({});
+
+const CharacterAnimationsProvider = (props) => {
+    const [animationIndex, setAnimationIndex] = useState(0);
+    const [animations, setAnimations] = useState([]);
+
+    return (
+        <CharacterAnimationsContext.Provider value={{
+            animationIndex,
+            setAnimationIndex,
+            animations,
+            setAnimations
+        }}>
+            {props.children}
+            </CharacterAnimationsContext.Provider>
+            );
+};
+
+export const useCharacterAnimations = () => {
+    return useContext(CharacterAnimationsContext);
+}
